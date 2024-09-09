9686e606fa41b619255b32feedc3abeb0b4ae47b
sunjun0914 <110458672+sunjun0914@users.noreply.github.com>
2024-09-09 19:44:30 +0900

    Update and rename main_05.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# main_05 : pdf파일 읽기 + splitting + embedding + db 저장 + 챗봇 기능 추가+ web 서비스
-from dotenv import load_dotenv
-load_dotenv()
-
+# main : pdf파일 읽기 + splitting + embedding + db 저장 + 챗봇 기능 추가+ web 서비스
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma  
@@ -12,9 +12,6 @@ from langchain.chains import RetrievalQA
 import os
 import streamlit as st
 import tempfile
-
-# Chroma DB가 저장된 디렉토리 경로
-persist_directory = './db/chromadb'
 
 # Streamlit 웹페이지 제목 설정
 st.title("ChatPDF")
@@ -32,13 +29,13 @@ def pdf_to_document(uploaded_file):
         f.write(uploaded_file.getvalue())  # 업로드된 파일을 저장
     loader = PyPDFLoader(temp_filepath)  # PDF 파일 로드
     pages = loader.load_and_split()  # 페이지별로 분리
-    return pages
+    return pages, temp_dir
 
 # 파일이 업로드되면 실행되는 코드
 if uploaded_file is not None:
     try:
         # PDF 파일을 로드하여 페이지별로 분리
-        pages = pdf_to_document(uploaded_file)
+        pages, temp_dir = pdf_to_document(uploaded_file)
 
         # 텍스트 분리 설정 (1000자씩 분리하고, 50자의 오버랩 적용)
         text_splitter = RecursiveCharacterTextSplitter(
@@ -52,20 +49,16 @@ if uploaded_file is not None:
         # 텍스트 임베딩을 위한 OpenAI Embeddings 모델 로드
         embeddings_model = OpenAIEmbeddings()
 
-        # Chroma 데이터베이스가 없으면 새로 생성하고, 있으면 불러오기
-        if not os.path.exists(persist_directory):
-            chromadb = Chroma.from_documents(
-                texts,  # 분리된 텍스트
-                embeddings_model,  # 임베딩 모델
-                collection_name = 'esg',  # 컬렉션 이름
-                persist_directory = persist_directory,  # 저장할 디렉토리 경로
-            )
-        else:
-            chromadb = Chroma(
-                persist_directory=persist_directory,  # 기존에 저장된 디렉토리 경로
-                embedding_function=embeddings_model,  # 임베딩 모델
-                collection_name='esg'  # 컬렉션 이름
-            )
+        # Chroma DB 설정 (임시 디렉토리 사용)
+        #chromadb = Chroma.from_documents(texts, embeddings_model)
+        persist_directory = os.path.join(temp_dir.name, 'db')
+        
+        chromadb = Chroma.from_documents(
+            texts,
+            embeddings_model,
+            collection_name='esg',
+            persist_directory=persist_directory,
+        )
 
         # 질문을 받을 수 있는 섹션 생성
         st.header("PDF에게 질문해보세요!!")
@@ -86,3 +79,8 @@ if uploaded_file is not None:
     except Exception as e:
         # 오류 발생 시 오류 메시지 출력
         st.error(f"An error occurred: {e}")
+
+    finally:
+        # 임시 디렉토리 정리
+        if 'temp_dir' in locals():
+            temp_dir.cleanup()
